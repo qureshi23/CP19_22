5e28af8701e4806e1057bd12a509ccf7776c447a
Kashif0070 <49775699+Kashif0070@users.noreply.github.com>
2019-04-20 23:41:22 +0500

    Computer programmung
    Assignment02 Q4

---
+++ CP.py
@@ -0,0 +1,34 @@
+A=int(input("Please enter an integar from 1 to 10: "))
+
+if A<1 or A>10:
+   print("invalid number")
+
+if A==1:
+   print("I")
+
+if A==2:
+   print("II")
+
+if A==3:
+   print("III")
+
+if A==4:
+   print("IV")
+
+if A==5: 
+   print("V")
+
+if A==6: 
+   print("VI")
+
+if A==7:
+   print("VII")
+
+if A==8:
+   print("VIII")
+
+if A==9:
+   print("IX")
+
+if A==10:
+   print("X")

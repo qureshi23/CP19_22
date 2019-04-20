08d3b839a6eb00eebb8f9c1b2a79b778e87bc787
YasirAli0057 <49761474+YasirAli0057@users.noreply.github.com>
2019-04-20 23:38:00 +0500

    CP01.py
    Ass02 Q2

---
+++ CP01.py
@@ -0,0 +1,27 @@
+R1=int(input("Please Enter the length of  rectangle1:" ))
+
+
+R2=int(input("please Enter the lenght of rectangle2:"))
+
+
+W1=int(input("Please Enter the width of rectangle1:" ))
+
+
+W2=int(input("Please Enter the width of rectangle2:" ))
+
+
+
+AR1=R1*W1
+
+
+
+AR2=R2*W2
+
+
+if AR1 > AR2: 
+
+   print("Rectangle1 has a greater Area")
+
+else: 
+   print("Rectangle2 has a lesser Area")
+ 
